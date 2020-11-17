b86677d4f5d536930428f3d8304fbeed089722cc
Sprk3lzZ <yanis.zge@gmail.com>
2020-11-17 19:23:49 +0100

    C. Un abri en carton !

--- messagePourSauverLeMonde.py
+++ messagePourSauverLeMonde.py
@@ -19,6 +19,35 @@ file_arbre.close()
 
 ############################################################
 
+file_abri = open('desProjectilesEtDesDodos.txt', "r")
+pas = 0
+gauche = 0
+droite = 0
+avant = 0
+
+for line3 in file_abri:
+    if 'rocher' in line3:
+        pas += 5
+        gauche += 5
+    if 'arbre' in line3:
+        pas += 10
+        avant += 10
+    if 'dodo' in line3:
+        pas += 6
+        droite += 6
+
+    if pas >= 100 and 'lac' in line3:
+        if droite > avant and droite > gauche:
+            max = "droite"
+        elif avant > droite and avant > gauche:
+            max = "avant"
+        else:
+            max = "gauche"
+        print("Vous avez fait un nombre de ",pas, " dont la pluspart vers la/l' : ", max)
+
+
+################################################################################################
+
 
 
 
